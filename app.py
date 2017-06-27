14914fa2fe4125c3d692218b5cc25a6e5fac8e47
Michael McCune <msm@redhat.com>
2017-06-27 12:12:17 -0400

    change default listen port to 8080

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def main():
     app = flask.Flask(__name__)
     app.add_url_rule('/',
         view_func=IndexView.as_view('index', args.host, args.port))
-    app.run()
+    app.run(port=8080)
 
 if __name__ == '__main__':
     main()
